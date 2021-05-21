5c05dc621e9294a9855160fad9d89d55a797cd1c
PriscoCoppola <priscocoppola7@gmail.com>
2021-05-21 13:02:07 +0200

    add bonus

--- AJAX/js/main.js
+++ AJAX/js/main.js
@@ -3,15 +3,35 @@ const root = new Vue({
     el: "#root",
     data: {
         discs: [],
+        artists: [],
+        selected: "All",
     },
     created() {
         axios
             .get("http://localhost:8888/php-ajax-dischi/AJAX/scripts/data.php")
             .then((res) => {
-                this.discs = res.data;
+                this.discs = res.data.filtered;
+                this.artists = res.data.artist;
+                this.artists.unshift("All");
             })
             .catch((err) => {
                 console.log("Errore", err);
             });
+    },
+    methods: {
+        getAlbumArtist() {
+            axios
+                .get(
+                    "http://localhost:8888/php-ajax-dischi/AJAX/scripts/data.php?query=" +
+                        this.selected
+                )
+                .then((res) => {
+                    this.discs = res.data.filtered;
+                    console.log(res.data);
+                })
+                .catch((err) => {
+                    console.log("Errore", err);
+                });
+        },
     },
 });
